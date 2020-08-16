2184d27450f21cfc6b5aa1b505c7b01e2aee1eb9
Kamal Teja <kamal@pop-os.localdomain>
2020-08-16 12:17:46 +0530

    Changed Bulk Download Link

--- resources/views/pages/dashboard/zipMail.blade.php
+++ resources/views/pages/dashboard/zipMail.blade.php
@@ -2,7 +2,7 @@
     <img src="https://xplore.co.in/img/xplore.png" width="200">
     <h3 style="margin-top: 5rem;font-weight: bold;">Hello!</h3>
     <h3 style="margin-top: 1rem;color: #696969;">This is the link containing all the certificates</h3>
-    <a href="http://certificate.in:8000/dashboard/zipFiles/{{$session_key}}" style="background: #34495e; color: #fff; text-decoration: none; margin-top: 1rem; padding: 0.5rem 1rem;border-radius: 5px;">Open</a>
+    <a href="{{url("/")}}/dashboard/zipFiles/{{$session_key}}" style="background: #34495e; color: #fff; text-decoration: none; margin-top: 1rem; padding: 0.5rem 1rem;border-radius: 5px;">Open</a>
     <hr style="margin: 2rem 0;">
-    <p style="color: #696969;">If you’re having trouble clicking the "Open" button, copy and paste the URL below into your web browser: http://certificate.in:8000/dashboard/zipFiles/{{$session_key}}</p>
+    <p style="color: #696969;">If you’re having trouble clicking the "Open" button, copy and paste the URL below into your web browser: {{url("/")}}/dashboard/zipFiles/{{$session_key}}</p>
 </div>
